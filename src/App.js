f1d4ce10342125a1c4dfa62a93a2f40aed4169e6
19Kushagra0 <19Kushagra0@gmail.com>
2026-05-20 16:13:29 +0530

    Kushagra.dev (#10)
    * Created react file
    
    * Add basic structure with few components
    
    feat: Added basic structure with few components – Header, Toolbar, Title, Elements, ColorPalette and TechUsed
    
    * Complete UI implementation with final layout and styling
    
    * _redirects file added
    
    * title updated
    
    * Update README.md
    
    Link Nitlify added
    
    * searchIcon added
    
    * increase responsiveness
    
    increase responsiveness for 375px
    
    * increase z-index of header
    
    main header  z-index increased
    
    * cursor: "pointer" added
    
    * fotter updated
    
    * Update Elements.css
    
    * navbar added
    
    * Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Scrollheader from "./myComponents/Scrollheader";
 import Header from "./myComponents/Header";
 import Toolbar from "./myComponents/Toolbar";
 import Title from "./myComponents/Title";
-import Lastest from "./myComponents/Latest";
 import Elements from "./myComponents/Elements";
 import ColorPalette from "./myComponents/ColorPalette";
 import TechUsed from "./myComponents/TechUsed";
